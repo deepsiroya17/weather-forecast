a9951c9273913d09dd2c7479a8e3b69cfdf7edb7
Deep Siroya <deepsiroya99@gmail.com>
2020-07-23 22:58:16 +0530

    Miner change

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.post("/", function(req, res) {
     });
 });
 
-
-app.listen(3000 || process.env.PORT, function() {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`Server running on port ${PORT}`);
 });
